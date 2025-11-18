21695dec28d2dffb844decc1d2e52db3da354050
Won Sangyun <wonsang4232@korea.ac.kr>
2025-11-18 15:12:17 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      // name: `comibear-log`,
-      // href: "https://github.com/comibear/comibear.github.io",
+      name: `Auto Trading`,
+      href: "https://github.com/comibear/CoinAutoTrading",
     },
   ],
   // blog setting (required)
